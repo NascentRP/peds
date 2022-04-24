53ea7319ae0733a1c76052abdced4c9a8cfffb75
Matt <matthewrorke@gmail.com>
2022-04-24 11:51:16 +0200

    do a location check and spawn/delete

--- client.lua
+++ client.lua
@@ -1,8 +1,33 @@
-Citizen.CreateThread(function()
-    DecorRegister("ScriptedPed", false)
-end)
+local spawnedPeds = {}
 
 function spawnPed(model, v, withTask)
+	table.insert(spawnedPeds, {model=model, location=v, task=withTask, isSpawned=false, ped = nil})
+	print(json.encode(spawnedPeds))
+end
+
+Citizen.CreateThread(function()
+    DecorRegister("ScriptedPed", false)
+
+	local range = 100.0
+	local ped = PlayerPedId()
+	while true do
+		local coords = GetEntityCoords(ped)
+		for k, v in pairs(spawnedPeds) do
+			local diff = #(coords - vector3(v.location.x, v.location.y, v.location.z))
+			if diff < range and not v.isSpawned then
+				spawnedPeds[k].isSpawned = true
+				spawnedPeds[k].ped = createNearbyPed(v.model, v.location, v.task)
+			elseif v.isSpawned and diff > range then
+				spawnedPeds[k].isSpawned = false
+				destroyPed(spawnedPeds[k].ped)
+				spawnedPeds[k].ped = nil
+			end
+		end
+		Wait(500)
+	end
+end)
+
+function createNearbyPed(model, v, withTask)
 	modelHash = GetHashKey(model)
 	RequestModel(modelHash)
 	while not HasModelLoaded(modelHash) do
@@ -19,5 +44,11 @@ function spawnPed(model, v, withTask)
 	DecorSetBool(created_ped, 'ScriptedPed', true)
 	SetModelAsNoLongerNeeded(modelHash)
 	return created_ped
+end
+
+function destroyPed(id)
+	if IsEntityAPed(id) then
+		DeletePed(id)
+	end
 end
 exports('spawnPed', spawnPed)
